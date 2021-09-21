a868eea5865ac85a1ba09ae4d200c701d61137de
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-09-21 16:37:49 +0200

    add responsive image layout

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import componentImg from '../public/img/img03.jpg'
 
 export default function Home() {
   return (
     <div className="Home">
       <div className="main-wrapper wrapper">
         <h1 className="sub-title text-2xl text-center">L'excellence de la tradition française au service de votre élégance</h1>
+        <Image layout="responsive" src={componentImg} alt="Picture of shop" />
       </div>
     </div>
   )
