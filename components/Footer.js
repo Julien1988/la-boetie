f17c9954989d4f9da7df244dc8c29895bc367741
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-09-21 16:11:51 +0200

    change instagram icone font-size

--- components/Footer.js
+++ components/Footer.js
@@ -6,32 +6,30 @@ import { FaInstagram, FaFacebookF } from 'react-icons/fa';
 
 const Footer = () => (
     <footer className='footer'>
-      <div className='footer-wrapper'>
-        <nav className="nav flex flex-col">
-          <ul>
-            <li>
+      <div className='footer-wrapper wrapper'>
+        <nav className="nav">
+          <ul className="flex flex-col">
+            <li className="flex justify-end">
               <p>19, rue de la Boétie 75008 Paris</p>
             </li>
-            <li>
+            <li className="flex justify-end">
               <Link href="tel:+33142650716">
                 <a>+33 1 42 65 07 16</a>
               </Link>
             </li>
-            <li>
+            <li className="flex justify-end">
               <Link href="mailto:contact@maisonlaboetie.com">
                 <a>contact@maisonlaboetie.com</a>
               </Link>
             </li>
-            <li>
+            <li className="flex justify-end flex-row">
               <Link href="https://www.instagram.com/maisonlaboetie/">
-                <a>
+                <a className="pr-1 text-lg">
                   <FaInstagram />
                 </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.facebook.com/maisonlaboetie">
-                <a>
+            </Link>
+            <Link href="https://www.facebook.com/maisonlaboetie">
+                <a className="pl-1">
                   <FaFacebookF />
                 </a>
               </Link>
--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,9 @@
 import Link from 'next/link';
 
-//import Image from 'next/Image';
-
-
 const Header = () => {
     return (
         <header className="header">
-            <div className="header-wrapper">
+            <div className="header-wrapper wrapper">
                 <nav className="navbar text-center text-6xl">
                     <Link href="/">
                         <a className="title">LA BOÉTIE</a>
