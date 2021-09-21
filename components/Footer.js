56dc78be8823fa21a91bea781e198f670bb4752b
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-09-21 15:56:38 +0200

    work on footer icones & list in nav

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,42 @@
 import Link from 'next/link';
+// https://react-icons.github.io/react-icons/icons?name=fa
+import { FaInstagram, FaFacebookF } from 'react-icons/fa';
+
+
 
 const Footer = () => (
     <footer className='footer'>
       <div className='footer-wrapper'>
-        <nav className="nav">
-        <p>19, rue de la Boétie 75008 Paris</p>
-        <Link href="#">
-          <a>+33 1 42 65 07 16</a>
-        </Link>
-        <Link href="#">
-          <a>contact@maisonlaboetie.com</a>
-        </Link>
+        <nav className="nav flex flex-col">
+          <ul>
+            <li>
+              <p>19, rue de la Boétie 75008 Paris</p>
+            </li>
+            <li>
+              <Link href="tel:+33142650716">
+                <a>+33 1 42 65 07 16</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="mailto:contact@maisonlaboetie.com">
+                <a>contact@maisonlaboetie.com</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.instagram.com/maisonlaboetie/">
+                <a>
+                  <FaInstagram />
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.facebook.com/maisonlaboetie">
+                <a>
+                  <FaFacebookF />
+                </a>
+              </Link>
+            </li>
+          </ul>
         </nav>
       </div>
     </footer>
