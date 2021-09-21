04ee054b9c79601822c973ec28fa309bb34f061b
Julien Broens <julien-broens@MBP-de-Julien.lan>
2021-09-21 16:23:02 +0200

    add img

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,10 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-     <h1 className="sub-title text-2xl">L'excellence de la tradition française au service de votre élégance</h1>
+    <div className="Home">
+      <div className="main-wrapper wrapper">
+        <h1 className="sub-title text-2xl text-center">L'excellence de la tradition française au service de votre élégance</h1>
+      </div>
     </div>
   )
 }
